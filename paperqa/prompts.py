3c966a03431c8538c8a833533642dcaefb06d674
shifang11 <137263668+shifang11@users.noreply.github.com>
2023-07-07 16:10:17 +0800

    Update prompts.py
    取消prompt引用说明

--- paperqa/prompts.py
+++ paperqa/prompts.py
@@ -24,7 +24,7 @@ qa_prompt = PromptTemplate(
     "If the context provides insufficient information, "
     'reply "I cannot answer". '
     "For each part of your answer, indicate which sources most support it "
-    "via valid citation markers at the end of sentences, like (Example2012). "
+    #"via valid citation markers at the end of sentences, like (Example2012). "
     "Answer in an unbiased, comprehensive, and scholarly tone. "
     "If the question is subjective, provide an opinionated answer in the concluding 1-2 sentences. \n\n"
     "{context}\n"
